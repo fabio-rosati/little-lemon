343843188f92d552cce39a9721040ce43e5c1738
Fabio Rosati <fabio.rosati89@gmail.com>
2023-01-31 20:48:41 +0100

    Onboarding screen

--- App.js
+++ App.js
@@ -1,20 +1,18 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { View, StyleSheet, SafeAreaView, Platform, StatusBar } from 'react-native';
+
+import OnboardingScreen from "./screens/OnboardingScreen";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <OnboardingScreen />
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+      flex: 1,
+      paddingTop: Platform.OS === "android" ? StatusBar.currentHeight : 0
   },
 });
---
+++ screens/OnboardingScreen.js
@@ -0,0 +1,147 @@
+import React, { useState } from 'react';
+import { View, Text, Image, Pressable, StyleSheet, TextInput, Alert, Platform } from 'react-native';
+import { validateEmail } from '../utils';
+import { validateName } from '../utils';
+
+export default function OnboardingScreen() {
+    const [name, onChangeName] = useState('');
+    const [email, onChangeEmail] = useState('');
+  
+    // Other solution
+    const validValues = validateEmail(email) && validateName(name)
+  
+    const nextScreen = () => {
+        onChangeName('')
+        onChangeEmail('')
+
+    //   Platform.OS === 'web'
+    //     ? alert('Thanks for subscribing, stay tuned!')
+    //     : Alert.alert('', 'Thanks for subscribing, stay tuned!')
+    };
+
+    return (
+        <View style={styles.container}>
+            {/* Header */}
+            <View style={styles.header}>
+                <Image
+                    style={styles.image}
+                    source={require('../assets/little-lemon-logo-and-title.png')}
+                    resizeMode="cover"
+                    accessible={true}
+                    accessibilityLabel={'Little Lemon Logo'}
+                />
+            </View>
+
+            {/* Body */}
+            <View style={styles.body}>
+                <Text style={styles.text1}>Let us get to know you</Text>
+
+                <Text style={styles.text}>First Name</Text>
+                <TextInput
+                    style={styles.inputBox}
+                    value={name}
+                    onChangeText={onChangeName}
+                    placeholder={'Type your name'}
+                />
+
+                <Text style={styles.text}>Email</Text>
+                <TextInput
+                    style={styles.inputBox}
+                    value={email}
+                    onChangeText={onChangeEmail}
+                    placeholder={'Type your email'}
+                    keyboardType={'email-address'}
+                />
+            </View>
+
+            {/* Footer */}
+            <View style={styles.footer}>
+                <Pressable 
+                    disabled={!validValues}
+                    style={
+                        validValues 
+                            ? styles.buttonEnabled
+                            : styles.buttonDisabled
+                    }
+                    onPress={nextScreen}
+                    >
+                    <Text style={styles.buttonText}>Next</Text>
+                </Pressable>
+            </View>
+        </View>
+      );
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        flexDirection: 'column',
+    },
+    header: {
+        // height: 90,
+        backgroundColor: '#dee3e9',
+        justifyContent: 'center', // Vertical
+        alignItems: 'center', // Horizontal
+        paddingVertical: 10,
+    },
+    body: {
+        flex: 1,
+        paddingHorizontal: 40,
+        paddingVertical: 40,
+        justifyContent: 'center',
+        backgroundColor: '#cbd2d9'
+    },
+    footer: {
+        paddingHorizontal: 40,
+        paddingVertical: 40,
+        backgroundColor: '#f1f4f7',
+    },
+    image: {
+        // width: 200,
+        // height: 200,
+    },
+    text: {
+        fontWeight: '500',
+        fontSize: 20,
+        color: '#344854',
+        textAlign: 'center',
+    },
+    text1: {
+        fontWeight: '500',
+        fontSize: 20,
+        color: '#344854',
+        textAlign: 'center',
+        marginBottom: 50,
+    },
+    buttonEnabled: {
+        justifyContent: 'center',
+        fontSize: 22,
+        padding: 5,
+        backgroundColor: '#cbd2d9',
+        borderRadius: 10,
+    },
+    buttonDisabled: {
+        justifyContent: 'center',
+        fontSize: 22,
+        padding: 5,
+        backgroundColor: '#cbd2d9',
+        borderRadius: 10,
+        opacity: 0.4,
+    },
+    buttonText: {
+        color: '#344854',
+        textAlign: 'center',
+        fontSize: 20,
+    },
+    inputBox: {
+        justifyContent: 'center',
+        borderWidth: 2,
+        padding: 10,
+        marginVertical: 10,
+        fontSize: 20,
+        placeholderTextColor: 'grey',
+        borderRadius: 10,
+        borderColor: '#344854',
+        // backgroundColor: '#EDEFEE',
+    },
+});
